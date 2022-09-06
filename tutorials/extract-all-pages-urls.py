4398d85fe043e4bbbed8b7f0db85acef7efd3c8b
bbaransahin <barana340@gmail.com>
2022-09-06 21:30:02 +0300

    Edited code for final usage and added some comments to better readability

--- tutorials/extract-all-pages-urls.py
+++ tutorials/extract-all-pages-urls.py
@@ -1,22 +1,21 @@
 import bs4
 import requests
-import pickle
 
-pages = []
-pages_visited = []
+pages = [] # All of the visited pages are saving to this list
+pages_visited = [] # Tracking to not to visit same page twice
 
 def extract_all_page_urls(address):
     print(address)
 
-    save_file = open("save.pkl","wb")
     pages_visited.append(address)
-    pickle.dump(pages_visited, save_file)
 
     url = address
     hdr = {'User-Agent': 'Mozilla/5.0'} # To prevent 403 response
     req = requests.get(url, headers=hdr)
     soup = bs4.BeautifulSoup(req.text, "html.parser")
-
+ 
+    # I was having problem while scraping so added a try except block to ignore pages that I have error
+    # These are usually files, for example when it tries to find links inside of zip file it terminates the program
     try:
         if (soup.find_all('a') == None):
             return None;
@@ -33,10 +32,6 @@ def extract_all_page_urls(address):
         print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
 extract_all_page_urls('https://bilisimvadisi.com.tr')
-
-print("=============================RESULTS=============================")
-for link in pages:
-    print(link)
 
 print("num pages:", len(pages))
 print("num pages visited:", len(pages_visited))
